f241da38244518536ae393a5ee2c2838e3e75694
riteme <riteme@qq.com>
2021-04-24 13:33:29 +0000

    Sync: 13f230a447019a0955f3ba4d3d71bbcc72e9d510

--- source/include/access.svh
+++ source/include/access.svh
@@ -8,16 +8,23 @@
 `define __ACCESS_SVH__
 
 `define STRUCT_ACCESSOR(struct_name, member_name, return_type) \
+    /* verilator lint_off VARHIDDEN */ \
     function return_type struct_name``_``member_name(struct_name e); \
         /* verilator public */ \
+        typedef return_type _return_type; \
         automatic logic _unused_ok = &{1'b0, e, 1'b0}; \
-        return return_type'(e.member_name); \
+        return _return_type'(e.member_name); \
     endfunction
+    /* verilator lint_on VARHIDDEN */
+
 `define STRUCT_TASK_ACCESSOR(struct_name, member_name, return_type) \
+    /* verilator lint_off VARHIDDEN */ \
     task struct_name``_``member_name(input struct_name e, output return_type o); \
         /* verilator public */ \
+        typedef return_type _return_type; \
         automatic logic _unused_ok = &{1'b0, e, 1'b0}; \
-        assign o = return_type'(e.r); \
+        assign o = _return_type'(e.r); \
     endtask
+    /* verilator lint_on VARHIDDEN */
 
 `endif
